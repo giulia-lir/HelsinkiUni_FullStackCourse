b04a9b999685183942f9aa5f16c4c135c0c39c62
giulia-lir <giulia.libe3@gmail.com>
2024-04-19 14:38:30 +0300

    Course info part 2.2

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -2,12 +2,18 @@ const Course = ( {course} ) => {
     
     const Header = ( {name} ) => <h1>{name}</h1>
     const Content = ( {parts} ) => parts.map(part => <Part key={part.id} part={part} />)
-    const Part = ( {part} ) => <p>{part.name} {part.exercises}</p> 
+    const Part = ( {part} ) => <p>{part.name} {part.exercises}</p>
+    const Total = ({ parts }) => {
+        const totalExercises = parts.reduce((total, part) => total + part.exercises, 0);
+        return <div><b>total of {totalExercises} exercises</b></div>;
+      }
+      
 
     return(
         <div>
             <Header name={course.name} />
             <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
     )
 }
